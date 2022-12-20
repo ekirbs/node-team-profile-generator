9df005f109903a844018727f1fbaa7f488898abf
Eric K <erickirberger@gmail.com>
2022-12-19 21:59:02 -0500

    added validation, labeled and cleaned some code

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ const managerQuestions = [
   }
 ]
 
+// ADD NEW EMPLOYEE QUESTION
 const addEmployee = [
   {
     type: 'list',
@@ -82,6 +83,7 @@ const addEmployee = [
   }
 ]
 
+// ENGINEER QUESTIONS
 const engineerQuestions = [
   {
     type: 'input',
@@ -137,6 +139,7 @@ const engineerQuestions = [
   }
 ]
 
+// INTERN QUESTION
 const internQuestions = [
   {
     type: 'input',
@@ -205,12 +208,16 @@ function init() {
         inquirer.prompt(engineerQuestions)
         .then (engineerData => {
           console.table(engineerData);
+          const newEngineer = new Engineer (engineerData);
+          teamArray.push(newEngineer);
           inquirer.prompt(addEmployee)
         })
       } else if (employeeChoice === 'Intern') {
         inquirer.prompt(internQuestions)
         .then (internData => {
           console.table(internData);
+          const newIntern = new Intern (internData);
+          teamArray.push(newIntern);
           inquirer.prompt(addEmployee)
         }) 
       } else {
@@ -225,6 +232,7 @@ function init() {
 
 init();
 
+// CONSTRUCTOR FUNCTION
 function Employee(data) {
   this.position = data.position
   this.name = data.name;
@@ -232,71 +240,3 @@ function Employee(data) {
   this.email = data.email;
   this.officeNum = data.officeNum
 }
-
-// const inquirer = require('inquirer');
-// const fs = require('fs');
-
-// const generateHTML = ({ name, location, github, linkedin }) =>
-//   `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-//   <title>Document</title>
-// </head>
-// <body>
-//   <header class="p-5 mb-4 header bg-light">
-//     <div class="container">
-//       <h1 class="display-4">Hi! My name is ${name}</h1>
-//       <p class="lead">I am from ${location}.</p>
-//       <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
-//       <ul class="list-group">
-//         <li class="list-group-item">My GitHub username is ${github}</li>
-//         <li class="list-group-item">LinkedIn: ${linkedin}</li>
-//       </ul>
-//     </div>
-//   </header>
-// </body>
-// </html>`;
-
-// inquirer
-//   .prompt([
-//     {
-//       type: 'input',
-//       name: 'name',
-//       message: 'What is your name?',
-//     },
-//     {
-//       type: 'input',
-//       name: 'location',
-//       message: 'Where are you from?',
-//     },
-//     {
-//       type: 'input',
-//       name: 'hobby',
-//       message: 'What is your favorite hobby?',
-//     },
-//     {
-//       type: 'input',
-//       name: 'food',
-//       message: 'What is your favorite food?',
-//     },
-//     {
-//       type: 'input',
-//       name: 'github',
-//       message: 'Enter your GitHub Username',
-//     },
-//     {
-//       type: 'input',
-//       name: 'linkedin',
-//       message: 'Enter your LinkedIn URL.',
-//     },
-//   ])
-//   .then((answers) => {
-//     const htmlPageContent = generateHTML(answers);
-
-//     fs.writeFile('index.html', htmlPageContent, (err) =>
-//       err ? console.log(err) : console.log('Successfully created index.html!')
-//     );
-//   });
